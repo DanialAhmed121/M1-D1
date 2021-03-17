fa57716d78d1236d9f2f29cf1d45991b76c0c014
DanialAhmed121 <80478593+DanialAhmed121@users.noreply.github.com>
2021-03-17 22:00:03 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,135 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+*/
+
+/* EXERCISE 1
+Create an array containing the first 5 positive numbers.
+*/
+const myArray = [
+    "banana",
+    "orange",
+    "apple",
+    "strawberry",
+    "peach"
+]
+console.log(myArray[0])
+
+/* EXERCISE 2
+Create an object containing your name, surname, email address and age.
+*/
+
+const human1 = {
+    name: "Danial",
+    surname: "Ahmed",
+    email: "youremail@example.com",
+    age: 20
+}
+
+// console.log(person.name)
+// console.log(person["name"])
+
+person.age = 21 // set person.age with value 21
+
+console.log(person);
+
+/* EXERCISE 3
+Add to the previously created object a boolean value to rappresent wheter you have or not a driving license.
+*/
+let individual = {
+    name: "Danial",
+    surname: "Ahmed",
+    email: "yourname@example.com",
+    age: 20,
+    HasDrivinglicense: false
+}
+
+/* EXERCISE 4
+Remove from the previously created object the age property.
+*/
+/* WRITE YOUR CODE HERE */
+let human = {
+    name: "Danial",
+    surname: "Ahmed",
+    email: "yourname@example.com",
+    HasDrivinglicense: False
+}
+/* EXERCISE 5
+Create a second object with name, surname, email address and verify that this object has a different email address than the previous one.
+*/
+
+/* WRITE YOUR CODE HERE */
+let Officer = {
+    name: "Frank",
+    surname: "Tenpenny",
+    email: "Pendalberry@example.com",
+    OfficerDrivinglicense: True
+}
+
+/* EXERCISE 6
+You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
+Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
+Write an algorithm that calculates the total cost to charge the user with.
+*/
+
+/* WRITE YOUR CODE HERE */
+let totalShoppingCart = 70
+ if (totalShoppingCart <= 50)
+ totalShoppingCart += 10 //
+ totalShoppingCart + totalShoppingCart + 10
+ console.log("Enjoy free shipping!")
+
+
+/* EXERCISE 7
+You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
+Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 8
+Create an object representing a car with properties like brand, model, licensePlate.
+After you create the first car, clone it 5 times and change the licensePlate for each cloned car without affecting the original one.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 9
+Create a new array called carsForRent containing all the cars from the previous exercise.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 10
+Remove the first and the last car from the carsForRent array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 11
+Print in the console the TYPES of the car variable, of its licensePlate and of the its brand properties.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 12
+Create a new array called carsForSale and insert 3 cars in it.
+Store in a variable totalCars the number of cars present in both carsForSale and carsForRent arrays.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 13
+Print in the console the data from each car in the carsForSale array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Send the code on the Eduflow platform. In the next days we'll also learn how to use GIT
+*/
